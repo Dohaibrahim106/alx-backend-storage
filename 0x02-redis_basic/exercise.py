93dce940b8ecfecf90bba13a71c7cc9460380b2c
Dohaibrahim106 <doha.ibrahim106@gmail.com>
2024-08-14 18:09:04 -0700

    commit

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,22 +1,25 @@
-ing the Redis NoSQL data storage.
+#!/usr/bin/env python3
+'''A module for using the Redis NoSQL data storage.
 '''
-import uuid
-import redis
 from functools import wraps
 from typing import Any, Callable, Union
+import redis
+import uuid
 
 
 def count_calls(method: Callable) -> Callable:
     '''Tracks the number of calls made to a method in a Cache class.
     '''
     @wraps(method)
-    def invoker(self, *args, **kwargs) -> Any:
-        '''Invokes the given method after incrementing its call counter.
+    def wrapper(self, *args, **kwargs) -> Any:
+        '''returns the given method after incrementing its call counter.
         '''
+
         if isinstance(self._redis, redis.Redis):
             self._redis.incr(method.__qualname__)
         return method(self, *args, **kwargs)
-    return invoker
+
+    return wrapper
 
 
 def call_history(method: Callable) -> Callable:
@@ -65,15 +68,14 @@ def replay(fn: Callable) -> None:
 class Cache:
     '''Represents an object for storing data in a Redis data storage.
     '''
+
     def __init__(self) -> None:
-        '''Initializes a Cache instance.
-        '''
         self._redis = redis.Redis()
         self._redis.flushdb(True)
 
     @call_history
     @count_calls
-    def store(self, data: Union[str, bytes, int, float]) -> str:
+    def store(self, data:  Union[str, bytes, int, float]) -> str:
         '''Stores a value in a Redis data storage and returns the key.
         '''
         data_key = str(uuid.uuid4())
